d5604bba8f34be8d30c1a9413fd031eee28099ac
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2024-01-11 16:06:04 -0300

    Estilização do Componente Banner

--- agencia_de_design/src/componentes/Banner/index.js
+++ agencia_de_design/src/componentes/Banner/index.js
@@ -1,18 +1,20 @@
 import imgBanner from '../../assets/banner.png';
 
-export default function Banner(){
+import './style.css';
+
+export default function Banner(props){
     return(
-        <div>
-            <figure>
+        <div className="banner">
+            <figure className={`imgBanner ${props.fundo}`}>
                 <img src={imgBanner} alt=""/>
             </figure>
-            <div>
-                <p>
-                    BRANGING / UI / UX / TECNOLOGIA
+            <div className="info-banner">
+                <p className='subTitulo'>
+                    B R A N G I N G  /  U I  /  U X  /  T E C N O L O G I A
                 </p>
-                <h1>
+                <p className='textoDestaque'>
                     Agência de Branding <span>e design digital</span>
-                </h1>
+                </p>
             </div>
         </div>
     )
