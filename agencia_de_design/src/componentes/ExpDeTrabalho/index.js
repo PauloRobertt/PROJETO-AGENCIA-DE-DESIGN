2039fc96eefcf8d28febb4e6beb7a419f286b043
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2024-01-11 16:05:40 -0300

    Estilização do Componente ExpDeTrabalho

--- agencia_de_design/src/componentes/ExpDeTrabalho/index.js
+++ agencia_de_design/src/componentes/ExpDeTrabalho/index.js
@@ -1,31 +1,38 @@
 import Card from '../Card';
 
-export default function ExpDeTrabalho(){
+import './style.css';
+
+export default function ExpDeTrabalho(props){
     return(
-        <div>
-            <h2>Experiências De Trabalho</h2>
-            <p>
-                Há mais de 10 anos no mercado de Branding, Desing Gráfico, Criação de Sites e Marketing Digital, nos empenhamos diariamente para entregar resultados que tragam impacto aos nossos clientes. 
-            </p>
-            <div>
-                <Card
-                    data="JUNHO 2012 - 2016"
-                    titulo="Web Designer"
-                    subTitulo="Pied Piper StartUp."
-                    descricao="Criação de Landing pages, sites institucionais e E-commerces completamente personalizados e otimizados para buscadores"
-                />
-                <Card
-                    data="AGOSTO 2016 - 2019"
-                    titulo="Product Designer"
-                    subTitulo="E Corp."
-                    descricao="Criação de modelos estratégicos de conversão identificando o cliente e mapeando toda a sua jornada de compra"
-                />
-                <Card
-                    data="FEVEREIRO 2019 - 2021"
-                    titulo="Digital Consulting"
-                    subTitulo="Arasaka Inc."
-                    descricao="Consultoria em estratégias digitais para todas as  etapas do ciclo do projeto, desde a definição inicial até a execução"
-                />
+        <div className={`Exp ${props.fundo} ${props.corDiferente}`}>
+            <div className="Experiencia">
+                <div className='titulo'>
+                    <h1>Experiências De Trabalho</h1>
+                    <hr></hr>
+                </div>
+                <p className="descricao">
+                    Há mais de 10 anos no mercado de Branding, Desing Gráfico, Criação de Sites e Marketing Digital, nos empenhamos diariamente para entregar resultados que tragam impacto aos nossos clientes.
+                </p>
+                <div className="card">
+                    <Card
+                        data="JUNHO 2012 - 2016"
+                        titulo="Web Designer"
+                        subTitulo="Pied Piper StartUp."
+                        descricao="Criação de Landing pages, sites institucionais e E-commerces completamente personalizados e otimizados para buscadores"
+                    />
+                    <Card
+                        data="AGOSTO 2016 - 2019"
+                        titulo="Product Designer"
+                        subTitulo="E Corp."
+                        descricao="Criação de modelos estratégicos de conversão identificando o cliente e mapeando toda a sua jornada de compra"
+                    />
+                    <Card
+                        data="FEVEREIRO 2019 - 2021"
+                        titulo="Digital Consulting"
+                        subTitulo="Arasaka Inc."
+                        descricao="Consultoria em estratégias digitais para todas as  etapas do ciclo do projeto, desde a definição inicial até a execução"
+                    />
+                </div>
             </div>
         </div>
     )
