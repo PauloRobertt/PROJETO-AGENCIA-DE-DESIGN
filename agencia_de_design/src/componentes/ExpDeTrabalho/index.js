1cb8687a835d341fd6fbb70fca6afd2a8c1a3675
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2024-01-12 15:31:05 -0300

    Modificação no componente ExpDeTrabalho
    Pequena modificação no nome da classe .

--- agencia_de_design/src/componentes/ExpDeTrabalho/index.js
+++ agencia_de_design/src/componentes/ExpDeTrabalho/index.js
@@ -13,7 +13,7 @@ export default function ExpDeTrabalho(props){
                 <p className="descricao">
                     Há mais de 10 anos no mercado de Branding, Desing Gráfico, Criação de Sites e Marketing Digital, nos empenhamos diariamente para entregar resultados que tragam impacto aos nossos clientes.
                 </p>
-                <div className="card">
+                <div className="cards">
                     <Card
                         data="JUNHO 2012 - 2016"
                         titulo="Web Designer"
