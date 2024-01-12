75e1b6dbaf29da2f2004da9d13e59db3b7729549
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2024-01-12 15:24:28 -0300

    Estruturação da página concluída

--- agencia_de_design/src/App.js
+++ agencia_de_design/src/App.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 import Header from './componentes/Topo';
 import Banner from './componentes/Banner';
 import ExpDeTrabalho from './componentes/ExpDeTrabalho';
 import Footer from './componentes/Footer';
 
+import moon from './assets/moon.png';
+import sun from './assets/sun.png'
+
+import './App.css';
+
 function App(){
+
+  const [fundo, SetFundo] = useState("fundo-branco");
+  const [corDiferente, SetCorDiferente] = useState("corFundoBranco");
+  const [iconeBotao, SetIconeBotao] = useState(moon);
+
+    const mudarFundo = () =>{
+        if (fundo === "fundo-branco") {
+            SetFundo("fundo-preto");
+            SetCorDiferente("corFundoPreto");
+            SetIconeBotao(sun);
+        } else {
+            SetFundo("fundo-branco");
+            SetCorDiferente("corFundoBranco");
+            SetIconeBotao(moon);
+        }          
+    }
+
   return(
     <div className='App'>
-      <Header/>
-      <Banner/>
-      <ExpDeTrabalho/>
-      <Footer/>
+      <Header botao={mudarFundo} fundo={fundo} imgButton={iconeBotao}/>
+      <Banner fundo={fundo}/>
+      <ExpDeTrabalho fundo={fundo} corDiferente={corDiferente}/>
+      <Footer fundo={fundo}/>
     </div>
   )
 }
