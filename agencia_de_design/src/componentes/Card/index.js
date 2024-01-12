49a0cfd6f9e846043ad0a0f6ba39aa7abd2200ad
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2024-01-12 15:28:05 -0300

    Modificação no componente Card
    Substituição de uma tag h4 para uma tag h3.

--- agencia_de_design/src/componentes/Card/index.js
+++ agencia_de_design/src/componentes/Card/index.js
@@ -1,8 +1,10 @@
+import './style.css';
+
 export default function Card(props){
     return(
-        <div>
+        <div className="card">
             <p>{props.data}</p>
-            <h4>{props.titulo}</h4>
+            <h3>{props.titulo}</h3>
             <p>{props.subTitulo}</p>
             <p>{props.descricao}</p>
         </div>
